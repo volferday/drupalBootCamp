20e396672c14ffbd1cd7400e01179e6ba34dc0ab
Roman Tkachenko <r.tkachenko@dev-branch.com>
2022-05-03 17:39:47 +0300

    4677 created and configured webpack.config file

---
+++ pulsesProClearDrupal/docker4drupal/web/themes/custom/volftheme/src/bundle.js
@@ -0,0 +1,7 @@
+import '../css/style.css';
+import '../css/style-50.css';
+import '../scss/_variables_bootstrap.scss';
+import '../scss/_variables_drupal.scss';
+import '../scss/style.scss';
+import '../scss/my.scss';
+
